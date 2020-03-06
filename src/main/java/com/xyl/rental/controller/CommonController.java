738c3901e5665a66a9f0c2c8c293f60809334ad5
yanlin.xiang <yanlin.xiang@chinacreator.com>
2020-03-06 15:19:43 +0800

    commit 图片上传 xyl

---
+++ src/main/java/com/xyl/rental/controller/CommonController.java
@@ -0,0 +1,26 @@
+package com.xyl.rental.controller;
+
+import com.xyl.rental.service.PicUploadFileSystemService;
+import com.xyl.rental.vo.PicUploadResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("common")
+public class CommonController {
+    @Autowired
+    private PicUploadFileSystemService picUploadService;
+
+    /**
+     * @param uploadFile
+     * * @return
+     ** @throws Exception
+     * */
+    @PostMapping
+    @ResponseBody
+    public PicUploadResult upload(
+            @RequestParam("file") MultipartFile uploadFile) throws Exception {
+        return this.picUploadService.upload(uploadFile);
+    }
+}
